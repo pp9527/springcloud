abb346320146706b0d661c3a0f102ea3d8b82e64
pp9527 <2382852105@qq.com>
2022-09-29 15:43:37 +0800

    创建cloud-consumer-order80模块

---
+++ springcloud/cloud-consumer-order80/src/main/java/com/OrderMain80.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: pwz
+ * @create: 2022/9/29 15:41
+ * @Description:
+ * @FileName: OrderMain80
+ */
+@SpringBootApplication
+public class OrderMain80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class, args);
+    }
+}
