91596c2cf445110bde0c946ed1a7ac008ae1f969
pp9527 <2382852105@qq.com>
2022-09-30 12:23:28 +0800

    构建Eureka集群

--- payment.sql
+++ payment.sql
@@ -4,7 +4,7 @@ Source Host: localhost
 Source Database: payment
 Target Host: localhost
 Target Database: payment
-Date: 2022/9/29 11:11:12
+Date: 2022/9/30 12:03:24
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -13,10 +13,14 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 CREATE TABLE `payment` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `serial` varchar(200) DEFAULT '',
+  `serial` varchar(200) CHARACTER SET utf8 DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records 
 -- ----------------------------
+INSERT INTO `payment` VALUES ('1', '哈哈');
+INSERT INTO `payment` VALUES ('2', '1111');
+INSERT INTO `payment` VALUES ('3', '123');
+INSERT INTO `payment` VALUES ('4', '333');
