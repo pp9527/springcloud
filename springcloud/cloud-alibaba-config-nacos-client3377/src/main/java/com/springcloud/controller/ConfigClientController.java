95ad9304eda5f7962d9b82ed7664c5ce035bf6c3
pp9527 <2382852105@qq.com>
2022-10-08 15:05:50 +0800

    Nacos服务配置中心

---
+++ springcloud/cloud-alibaba-config-nacos-client3377/src/main/java/com/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: pwz
+ * @create: 2022/10/8 14:47
+ * @Description:
+ * @FileName: ConfigClientController
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
